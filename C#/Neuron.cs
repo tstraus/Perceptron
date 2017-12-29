57e29c1e0b64a70fa3698c63375deaecb111eeb8
tstraus <tckrsstrsbgh@gmail.com>
2017-12-29 13:40:11 -0500

    added C# version

---
+++ C#/Neuron.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PerceptronSharp
+{
+    public class Neuron
+    {
+        public Neuron(int numInputs)
+        {
+            weights = new List<double>();
+            for (int i = 0; i < numInputs; i++)
+                weights.Add(1.0 / numInputs);
+
+            threshold = 0.9;
+            learningRate = 0.01;
+        }
+
+        public double Forward(List<bool> inputs)
+        {
+            if (inputs.Count == weights.Count)
+            {
+                double sum = 0.0;
+
+                for (int i = 0; i < inputs.Count; i++)
+                    sum += Convert.ToDouble(inputs[i]) * weights[i];
+
+                return Math.Tanh(sum);
+            }
+
+            return -1;
+        }
+
+        public bool ForwardBinary(List<bool> inputs)
+        {
+            if (Forward(inputs) > threshold)
+                return true;
+
+            return false;
+        }
+
+        public bool ForwardBinaryCheck(List<bool> inputs, bool answer)
+        {
+            if (ForwardBinary(inputs) == answer)
+                return true;
+
+            return false;
+        }
+
+        public bool Train(List<bool> inputs, bool answer)
+        {
+            bool result = ForwardBinary(inputs);
+
+            var dAnswer = Convert.ToDouble(answer);
+            var dResult = Convert.ToDouble(result);
+            var delta = dAnswer - dResult;
+
+            for (int i = 0; i < inputs.Count; i++)
+                weights[i] += learningRate * delta * Convert.ToDouble(inputs[i]);
+
+            threshold -= learningRate * delta;
+
+            if (result == answer)
+                return true;
+
+            return false;
+        }
+
+        public List<double> weights;
+
+        private double threshold;
+
+        private double learningRate;
+    }
+}
---
+++ C#/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PerceptronSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+
+            int numInputs = 2;
+            ulong reps = 500;
+
+            Neuron neuron = new Neuron(numInputs);
+
+            var inputs = new List<bool>();
+            var results = new List<bool>();
+
+            for (ulong rep = 0; rep < reps; rep++)
+            {
+                for (int i = 0; i < numInputs; i++)
+                    inputs.Add(rand.NextDouble() > 0.5);
+
+                bool answer = inputs[0];
+                if (numInputs > 0)
+                {
+                    for (int i = 0; i < numInputs; i++)
+                        //answer = answer || inputs[i]; // OR Gate
+                        answer = answer && inputs[i]; // AND Gate
+
+                    answer = !answer; // NAND Gate
+				}
+
+                results.Add(neuron.Train(inputs, answer));
+                inputs.Clear();
+            }
+
+            //foreach (var result in results)
+                //Console.WriteLine($"result: {result}");
+
+            //Console.WriteLine("---------------------------------");
+			// OR Gate
+			//Console.WriteLine("correct: " + neuron.ForwardBinaryCheck(new List<bool> { false, false }, false));
+			//Console.WriteLine("correct: " + neuron.ForwardBinaryCheck(new List<bool> { false, true }, true));
+			//Console.WriteLine("correct: " + neuron.ForwardBinaryCheck(new List<bool> { true, false }, true));
+			//Console.WriteLine("correct: " + neuron.ForwardBinaryCheck(new List<bool> { true, true }, true));
+
+			// AND Gate
+			//Console.WriteLine("correct: " + neuron.ForwardBinaryCheck(new List<bool> { false, false }, false));
+			//Console.WriteLine("correct: " + neuron.ForwardBinaryCheck(new List<bool> { false, true }, false));
+			//Console.WriteLine("correct: " + neuron.ForwardBinaryCheck(new List<bool> { true, false }, false));
+			//Console.WriteLine("correct: " + neuron.ForwardBinaryCheck(new List<bool> { true, true }, true));
+
+			// NAND Gate
+			Console.WriteLine("correct: " + neuron.ForwardBinaryCheck(new List<bool> { false, false }, true));
+			Console.WriteLine("correct: " + neuron.ForwardBinaryCheck(new List<bool> { false, true }, true));
+			Console.WriteLine("correct: " + neuron.ForwardBinaryCheck(new List<bool> { true, false }, true));
+			Console.WriteLine("correct: " + neuron.ForwardBinaryCheck(new List<bool> { true, true }, false));
+        }
+    }
+}
